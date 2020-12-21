0d9b2876840363528a5d03c756db326b4bac9fd3
Budhi <sapiderman.lumpat@gmail.com>
2020-12-21 12:39:32 +0700

    test: add json schema testing 1. add chai-json-schema lib 2. add schema to test json stuff

--- test/unit/controller/health.spec.js
+++ test/unit/controller/health.spec.js
@@ -1,5 +1,6 @@
 'use strict';
-const { expect } = require('chai');
+const chai = require('chai');
+const { expect } = chai;
 const { health } = require('../../../src/controller');
 const express = require('express');
 const request = require('supertest');
@@ -7,11 +8,35 @@ const app = express();
 
 describe('GET /health', () => {
 
+    const healthSchema = {
+        title: 'Health schema',
+        type: 'object',
+        required: ['ok', 'health', 'name', 'version', 'timestamp', 'osUpTime', 'freemem', 'checks'],
+        properties: {
+            ok: {
+                type: 'boolean',                
+            },
+            health: {
+                type: 'string',
+            },
+            name: { type: 'string' },
+            version: { type: 'string' },
+            timestamp: { type: 'number' },
+            osUpTime: { type: 'number' },
+            freemem: { type: 'string' },
+            checks: { type: 'array' },
+        },
+    }
+   
     app.use("/health", health);
+    chai.use(require('chai-json-schema'));
 
     it('should return json health', async () => {
         const result = await request(app).get('/health');
 
-        expect(result.status).to.equal(200);
+        expect(result.status).to.equal(200);              
+        expect(result.body.ok).to.be.true;
+        expect(result.body.health).to.eql('ALIVE!');
+        expect(result.body).to.be.jsonSchema(healthSchema); 
     });
 });
--- test/unit/controller/hello.spec.js
+++ test/unit/controller/hello.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 const { expect } = require('chai');
-const { hello } = require('../../../src/controller');
+const { hello, bye } = require('../../../src/controller');
 const path = require('path');
 const express = require('express');
 const request = require('supertest');
@@ -10,12 +10,21 @@ const app = express();
 describe('GET /hello', () => {
 
     app.use("/v1/hello", hello);
+    app.use("/v1/bye", bye);
     app.set('views', path.join(__dirname, '../../../src/views'));
     app.set('view engine', 'pug');
 
-
-    it('should return json health', async () => {
+    it('should render a hello page', async () => {
         const result = await request(app).get('/v1/hello');
         expect(result.status).to.equal(200);
+        expect(result.res.text).not.empty;  // check the renders exist
+
     });
+
+    it('should render a bye page', async () => {
+        const result = await request(app).get('/v1/bye');
+        expect(result.status).to.equal(200);
+        expect(result.res.text).not.empty;  // check the renders exist
+    });
+
 });
